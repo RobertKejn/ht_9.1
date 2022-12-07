a6d6de0b624ffa4087d22eea71e2008340ddb766
Robert Kein <robertkejn@gmail.com>
2022-12-07 21:48:38 +0400

    Fixed == operator

--- ht_9.1/ht_9.1.cpp
+++ ht_9.1/ht_9.1.cpp
@@ -7,6 +7,26 @@ private:
 	int numerator_;
 	int denominator_;
 
+	int find_nod(int a, int b) {
+		while (a != 0 && b != 0) {
+			if (a > b) a = a % b;
+			else b = b % a;
+		}
+		return a + b;
+	}
+
+	void shortcut(Fraction& fr) {
+		int nod = find_nod(fr.numerator_, fr.denominator_);
+		if (nod != 1) {
+			fr.numerator_ = fr.numerator_ / nod;
+			fr.denominator_ = fr.denominator_ / nod;
+		}
+		if (fr.denominator_ < 0) {
+			fr.numerator_ *= -1;
+			fr.denominator_ *= -1;
+		}
+	}
+
 public:
 	Fraction(int numerator, int denominator)
 	{
@@ -15,9 +35,9 @@ public:
 	}
 
 	bool operator==(Fraction& fr) {
-		double a = numerator_ / static_cast<double>(denominator_);
-		double b = fr.numerator_ / static_cast<double>(fr.denominator_);
-		return a == b;
+		shortcut(*this);
+		shortcut(fr);
+		return this->numerator_ == fr.numerator_ && this->denominator_ == fr.denominator_;
 	}
 
 	bool operator!=(Fraction& fr) {
@@ -52,7 +72,7 @@ public:
 int main()
 {
 	Fraction f1(4, 3);
-	Fraction f2(6, 11);
+	Fraction f2(12, 9);
 
 	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
 	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
